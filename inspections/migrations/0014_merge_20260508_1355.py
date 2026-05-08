d7a4cfe8ad91f0b17e1f85450c7ff236ad6f9e95
Edgar Butwilowski <edgar.butwilowski@win.ch>
2026-05-08 16:20:37 +0200

    Migrationskonflikt auflösen

---
+++ inspections/migrations/0014_merge_20260508_1355.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("inspections", "0002_defect_inspection_answer"),
+        ("inspections", "0013_alter_defect_options_alter_defectimage_options_and_more"),
+    ]
+
+    operations = []
