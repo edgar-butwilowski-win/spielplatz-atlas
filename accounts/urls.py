0658b4045ecac64deb883230566fbd94a9c76f85
Edgar Butwilowski <edgar.butwilowski@win.ch>
2026-05-11 11:37:57 +0200

    Login- und Logout-Routen ergänzen

---
+++ accounts/urls.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2026 Fachstelle Geoinformation
+# Author: Edgar Butwilowski
+# All rights reserved.
+#
+# This source code is the property of the copyright holder.
+# Unauthorized copying, modification, distribution, or use is prohibited
+# unless expressly permitted in writing.
+
+from django.urls import path
+
+from . import views
+
+app_name = "accounts"
+
+urlpatterns = [
+    path("login/", views.SpielplatzAtlasLoginView.as_view(), name="login"),
+    path("logout/", views.logout_view, name="logout"),
+]
