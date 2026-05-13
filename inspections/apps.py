a5263dae2a2c1debef5bb9bf9c4fd4e5dcc62fd4
Edgar Butwilowski <edgar.butwilowski@win.ch>
2026-05-13 12:34:25 +0200

    Provisorische Dringlichkeitsregistrierung entfernen

--- inspections/apps.py
+++ inspections/apps.py
@@ -15,35 +15,4 @@ class InspectionsConfig(AppConfig):
     verbose_name = "Kontrollen"
 
     def ready(self):
-        self.register_defect_urgency_field()
-
         import inspections.signals  # noqa: F401
-
-    def register_defect_urgency_field(self):
-        from django.db import models
-
-        from inspections.models import Defect
-
-        if getattr(Defect, "_urgency_field_registered", False):
-            return
-
-        Defect.URGENCY_A = "a_immediate"
-        Defect.URGENCY_B = "b_medium_term"
-        Defect.URGENCY_CHOICES = [
-            (Defect.URGENCY_A, "A (sofort)"),
-            (Defect.URGENCY_B, "B (mittelfristig)"),
-        ]
-
-        if not hasattr(Defect, "urgency"):
-            Defect.add_to_class(
-                "urgency",
-                models.CharField(
-                    "Dringlichkeit",
-                    max_length=30,
-                    choices=Defect.URGENCY_CHOICES,
-                    blank=True,
-                    help_text="Nur erfassen, wenn ein Sicherheitsrisiko besteht.",
-                ),
-            )
-
-        Defect._urgency_field_registered = True
