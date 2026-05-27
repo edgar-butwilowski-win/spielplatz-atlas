c4f2dc5e65b312df7ed39cc8a0e9bf3fbf54b346
Edgar Butwilowski <edgar.butwilowski@win.ch>
2026-05-27 15:27:59 +0000

    I18N Runtime Install added

--- public/apps.py
+++ public/apps.py
@@ -10,4 +10,8 @@ from django.apps import AppConfig
 
 
 class PublicConfig(AppConfig):
-    name = 'public'
+    name = "public"
+
+    def ready(self):
+        from public import runtime_i18n
+        runtime_i18n.install()
