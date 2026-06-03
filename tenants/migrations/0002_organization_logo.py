c36f487e9dd6bef95dda2661c8053c627b71eb5b
Edgar Butwilowski <edgar.butwilowski@win.ch>
2026-06-03 18:15:41 +0200

    Add organization logo migration

---
+++ tenants/migrations/0002_organization_logo.py
@@ -0,0 +1,17 @@
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("media_assets", "0001_initial"),
+        ("tenants", "0001_initial"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="organization",
+            name="logo",
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name="organization_logos", to="media_assets.imageasset"),
+        ),
+    ]
