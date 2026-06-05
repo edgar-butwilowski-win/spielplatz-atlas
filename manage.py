8b3c3ba8de827213ecdb46a1b7d478ccf0d9c674
Edgar Butwilowski <edgar.butwilowski@win.ch>
2026-06-05 11:44:00 +0200

    Route createsuperuser to email admin command

--- manage.py
+++ manage.py
@@ -1,30 +1,20 @@
 #!/usr/bin/env python
-# Copyright (c) 2026 Fachstelle Geoinformation
-# Author: Edgar Butwilowski
-# All rights reserved.
-#
-# This source code is the property of the copyright holder.
-# Unauthorized copying, modification, distribution, or use is prohibited
-# unless expressly permitted in writing.
-
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+        raise ImportError("Django konnte nicht importiert werden. Ist die virtuelle Umgebung aktiv?") from exc
+
+    if len(sys.argv) > 1 and sys.argv[1] == "createsuperuser":
+        sys.argv[1] = "createemailadmin"
+
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
